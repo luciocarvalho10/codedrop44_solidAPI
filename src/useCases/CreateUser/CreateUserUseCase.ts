1c0cba63bdfbbe717e5fbf51c819498fd8ceba94
Lucio <luciocarvalho10@hotmail.com.br>
2021-10-16 19:54:05 -0400

    criando e usando MailProvider

--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,11 +1,13 @@
 import { User } from "../../entities/User";
+import { IMailProvider } from "../../providers/IMailProvider";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 import { ICreateUserRequestDTO } from "./CreateUserDTO";
 
 export class CreateUserUseCase {
 
   constructor(
-    private usersRepository: IUsersRepository
+    private usersRepository: IUsersRepository,
+    private mailProvider: IMailProvider
   ) {}
 
   async execute(data: ICreateUserRequestDTO) {
@@ -18,5 +20,18 @@ export class CreateUserUseCase {
     const user = new User(data)
 
     await this.usersRepository.save(user)
+
+    this.mailProvider.sendMail({
+      to: {
+        name: data.name,
+        address: data.email
+      },
+      from: {
+        name: data.name,
+        address: data.email
+      },
+      subject: 'Testando API REST com Node.js e TypeScript',
+      body: '<p>Teste você também, https://github.com/luciocarvalho10/codedrop44_solidAPI</p>'
+    })
   }
 }
